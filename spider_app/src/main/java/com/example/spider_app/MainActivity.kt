7394db49a35aaf88bd12ec32a1dc13d5ab239007
timeisthe <202408130230@stu.csust.edu.cn>
2026-02-24 10:13:46 +0800

    feature:添加测试ui

--- spider_app/src/main/java/com/example/spider_app/MainActivity.kt
+++ spider_app/src/main/java/com/example/spider_app/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.spider_app
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.v
 import android.util.Log.w
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dcelysia.csust_spider.core.KtorUtils
 import com.dcelysia.csust_spider.core.Resource
 import com.dcelysia.csust_spider.core.RetrofitUtils
+import com.dcelysia.csust_spider.edu.EducationRepository
 import com.dcelysia.csust_spider.education.data.remote.EducationHelper
 import com.dcelysia.csust_spider.education.data.remote.services.AuthService
 import com.dcelysia.csust_spider.education.data.remote.services.ExamArrangeService
+import com.dcelysia.csust_spider.login.edu.LoginRepository
+import com.dcelysia.csust_spider.login.sso.SsoRepository
 import com.dcelysia.csust_spider.mooc.data.remote.repository.MoocRepository
 import com.example.spider_app.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
@@ -22,57 +27,109 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        val loginButton = findViewById<Button>(R.id.loginButton)
+        val loginButton = findViewById<Button>(R.id.sso_loginButton)
         loginButton.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                RetrofitUtils.ClearClient("moocClient")
-                RetrofitUtils.ClearClient("EducationClient")
-                try {
-                    val loginResult = MoocRepository.instance.login("202408130230","@Wsl20060606")
-                    .filter { it !is Resource.Loading }
-                    .first()
-                    val sso_result = AuthService.Login("202408130230", "@Wsl20060606")
+                val response = SsoRepository.instance.login("202408130230", "@Wsl20060606")
+                val result = response.filter { it !is Resource.Loading }.first()
+                when (result) {
+                    is Resource.Success -> {
+                        Log.d(TAG, "sso登录成功")
+                    }
 
-                    if (sso_result&&(loginResult is Resource.Success)){
-                        val course = EducationHelper.getCourseScheduleByTerm("","2025-2026-1")
-                        Log.d(TAG,"course:${course}")
+                    is Resource.Error -> {
+                        Log.d(TAG, "sso登录失败:${result.msg}")
                     }
-                    else{
-                        Log.d(TAG,"登陆失败")
-                    }
-                } catch (e: Exception) {
-                    Log.d(TAG, e.toString())
+
+                    else -> {}
                 }
-                val result = ExamArrangeService.getExamArrange("2025-2026-1","期末考试")
+            }
+        }
+        val loginOutButton = findViewById<Button>(R.id.loginOutButton)
+        loginOutButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch{
+                val response = SsoRepository.instance.logout()
+                val result = response.filter { it !is Resource.Loading }.first()
+                when (result) {
+                    is Resource.Success -> {
+                        Log.d(TAG, "退出登录成功")
+                    }
+
+                    is Resource.Error -> {
+                        Log.d(TAG, "退出登录失败:${result.msg}")
+                    }
+
+                    else -> {}
+                }
+            }
+
+        }
+        binding.eduLoginButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = LoginRepository.instance.login()
+                val result = response.filter { it !is Resource.Loading }.first()
+                when (result) {
+                    is Resource.Success -> {
+                        Log.d(TAG, "教务登录成功")
+                    }
+
+                    is Resource.Error -> {
+                        Log.d(TAG, "教务登录失败:${result.msg}")
+                    }
+
+                    else -> {}
+                }
+            }
+        }
+        binding.eduGetScoreButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = EducationRepository.instance.getCourseGrades()
+                val result = response.filter { it !is Resource.Loading }.first()
                 when(result){
                     is Resource.Success -> {
-                        Log.d(TAG,"考试安排:${result.data}")
+                        Log.d(TAG, "获取成绩成功")
+                        val data = result.data
+                        Log.d(TAG, "获取到成绩：${data.data}")
                     }
                     is Resource.Error -> {
-                        Log.d(TAG,"考试安排:${result.msg}")
+                        Log.d(TAG, "获取成绩失败:${result.msg}")
                     }
-                    is Resource.Loading -> {
-                        Log.d(TAG,"考试安排:加载中")
-                    }
+                    else -> {}
                 }
             }
         }
-        binding.course.setOnClickListener {
+        binding.eduGetCoursesButton.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                val course = EducationHelper.getCourseScheduleByTerm("","2025-2026-1")
-                Log.d(TAG,"course:${course}")
+                val response = EducationRepository.instance.getCourseScheduleByTerm("","2024-2025-1")
+                val result = response.filter { it !is Resource.Loading }.first()
+                when(result){
+                    is Resource.Success->{
+                        Log.d(TAG, "获取课表成功")
+                        val data = result.data
+                        Log.d(TAG, "获取到课表：${data}")
+
+                    }
+                    is Resource.Error->{
+                        Log.d(TAG, "获取课表失败:${result.msg}")
+                    }
+                    else -> {
+                    }
+                }
             }
+
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding.campusGetElectricityButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+
+            }
+
         }
     }
+
 }
