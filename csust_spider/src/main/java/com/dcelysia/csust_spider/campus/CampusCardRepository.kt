2ff48e960683a877bc0b80486c05e55b17509e43
timeisthe <202408130230@stu.csust.edu.cn>
2026-02-25 09:02:20 +0800

    feature:删除不需要的文件

--- csust_spider/src/main/java/com/dcelysia/csust_spider/campus/CampusCardRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.dcelysia.csust_spider.campus
-
-class CampusCardRepository private constructor() {
-    companion object {
-        val instance by lazy { CampusCardRepository() }
-        const val TAG = "CampusCardRepository"
-    }
-
-}
